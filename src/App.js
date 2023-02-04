c8fdfe5a83c491c412ab30acb59044ae13a4dd85
Ali Hamdi <aawere1233@gmail.com>
2023-02-04 16:23:37 +0200

    2st commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Home from './components/homePage/Home';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
 
+      <Home />
 
+      <Footer />
     </div>
   );
 }
